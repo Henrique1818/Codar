54ae97531a65e9c8692780a82779b9faafbf0c69
henrique1818 <luiz.henrique01827@gmail.com>
2020-11-25 11:06:39 -0300

    logica do menu burguer

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const menuBtn = document.querySelector('.btn-menu')
+const body = document.querySelector('body')
 const show = document.querySelector('.navbar')
 let menuOpen = false
 
@@ -6,10 +7,12 @@ menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('action')
         show.classList.add('show')
+        body.classList.add('hide')
         menuOpen = true;
     } else {
         menuBtn.classList.remove('action')
         show.classList.remove('show')
+        body.classList.remove('hide')
         menuOpen = false;
     }
 })
